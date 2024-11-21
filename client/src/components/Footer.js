b68501fe69b7249b0422d4de74dc94c8a24d121b
Ojasva Raghav <Ojasraghav05@gmail.com>
2024-11-21 13:55:04 +0530

    3d

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,44 +1,65 @@
 import React from 'react';
-import "../styles/Footer.css"
+import "../styles/Footer.css";
+// If you're using React Router for internal links
+// import { Link } from 'react-router-dom';
+
 const Footer = () => {
   const currentYear = new Date().getFullYear();
 
+  const socialLinks = [
+    { href: "#", label: "Facebook", iconClass: "fa-facebook" },
+    { href: "#", label: "Twitter", iconClass: "fa-x-twitter" },
+    { href: "#", label: "Mastodon", iconClass: "fa-mastodon" },
+    { href: "#", label: "Instagram", iconClass: "fa-instagram" },
+  ];
+
+  const footerLinks = [
+    { href: "#", text: "Site Home" },
+    { href: "#", text: "Playground" },
+    { href: "#", text: "About" },
+    { href: "#", text: "Sitemap" },
+    { href: "#", text: "Contents" },
+  ];
+
   return (
-    <div>
-      <footer className="footer" role="contentinfo" itemScope itemType="http://schema.org/WPFooter">
-        <div className="footer__social" role="navigation" aria-labelledby="social-heading">
-          <h3 id="social-heading" className="sr-only">Follow us on social media</h3>
-          <a href="#" aria-label="Facebook">
-            <i className="fa-brands fa-facebook"></i>
-            <span className="sr-only">Facebook</span>
+    <footer className="footer" role="contentinfo" itemScope itemType="http://schema.org/WPFooter">
+      {/* Social Media Links */}
+      <div className="footer__social" role="navigation" aria-labelledby="social-heading">
+        <h3 id="social-heading" className="sr-only">Follow us on social media</h3>
+        {socialLinks.map(({ href, label, iconClass }) => (
+          <a
+            key={label}
+            href={href}
+            aria-label={label}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`footer__social-link ${iconClass}`}
+          >
+            <i className={`fa-brands ${iconClass}`} />
+            <span className="sr-only">{label}</span>
           </a>
-          <a href="#" aria-label="Twitter">
-            <i className="fa-brands fa-x-twitter"></i>
-            <span className="sr-only">Twitter</span>
-          </a>
-          <a href="#" aria-label="Mastodon">
-            <i className="fa-brands fa-mastodon"></i>
-            <span className="sr-only">Mastodon</span>
-          </a>
-          <a href="#" aria-label="Instagram">
-            <i className="fa-brands fa-instagram"></i>
-            <span className="sr-only">Instagram</span>
-          </a>
-        </div>
-        <hr className="footer__break" />
-        <ul className="footer__links" role="navigation" aria-labelledby="footer-links-heading">
-          <h3 id="footer-links-heading" className="sr-only">Footer Links</h3>
-          <li><a href="#">Site Home</a></li>
-          <li><a href="#">Playground</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Sitemap</a></li>
-          <li><a href="#">Contents</a></li>
-        </ul>
-        <p className="footer__copyright">
-          © {currentYear} SDavidPrince. Demo of a footer. Some Rights Reserved
-        </p>
-      </footer>
-    </div>
+        ))}
+      </div>
+
+      <hr className="footer__break" />
+
+      {/* Footer Links */}
+      <ul className="footer__links" role="navigation" aria-labelledby="footer-links-heading">
+        <h3 id="footer-links-heading" className="sr-only">Footer Links</h3>
+        {footerLinks.map(({ href, text }) => (
+          <li key={text}>
+            {/* Use <Link> if it's an internal route */}
+            {/* <Link to={href}>{text}</Link> */}
+            <a href={href}>{text}</a>
+          </li>
+        ))}
+      </ul>
+
+      {/* Copyright */}
+      <p className="footer__copyright">
+        © {currentYear} SDavidPrince. Demo of a footer. Some Rights Reserved
+      </p>
+    </footer>
   );
 };
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,13 +4,11 @@ const Navbar = () => {
   return (
     <div>
         <nav>
-            <a href="#" class="home">Brand</a>
-            <ul>
-                <li><a href="#">Link 1</a></li>
-                <li><a href="#">Link 2</a></li>
-                <li><a href="#">Link 3</a></li>
-                <li><a href="#">Link 4</a></li>
-                <li><a href="#">Profile</a></li>
+            <a href="#" class="home">Parking Management System</a>
+            <ul>    
+                <li><a href="#">MY ACCOUNT</a></li>
+                <li><a href="#">WALLET</a></li>
+                <li><a href="#">PROFILE</a></li>
             </ul>
         </nav>
     </div>
